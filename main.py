69734866b0d6944c9e8f0b7c4e1dd6f5bc5dc0d1
DillTheBeast <dillonmaltese@gmail.com>
2023-09-18 08:24:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# import time
+
+# site = 'https://discord.com/'
+
+# driver = webdriver.Chrome()
+# driver.get(site)
+# time.sleep(3)
+
+# # Locate element by visible text
+# toBrowser = driver.find_element(by=webdriver.common.by.By.XPATH, value="//*[text()='Open Discord in your browser']")
+# time.sleep(5)
+# elem = driver.find_element(by=webdriver.common.by.By.XPATH, value="//*[text()='Login']")
+# elem.send_keys("Brazil")
+# elem.send_keys(Keys.ENTER)
+# time.sleep(10)
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 import time
 
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.get("https://www.google.com/")
+site = 'https://discord.com/'
 
+driver = webdriver.Chrome() # specify the path if needed
+driver.get(site)
+
+# Wait until the specific element is present
 try:
-    #Wait for the condition inside parenthesis to happen before moving on
-    element = WebDriverWait(driver, 10).until(
-        #Locates the search bar
-        #Google homepage = q
-        EC.presence_of_element_located((By.NAME, "q"))
+    toBrowser = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//*[text()='Open Discord in your browser']"))
     )
-    element.send_keys("discord.com")
-    element.send_keys(Keys.ENTER)
+    toBrowser.click() # If you want to click the button
+
+    elem = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//*[text()='Login']"))
+    )
+    elem.click() # If you want to click the button
+    
+except:
+    print("Couldn't locate the element")
+
 finally:
-    time.sleep(4)
+    time.sleep(10)
     driver.quit()
